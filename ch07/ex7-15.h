e03fb928b5b5fa136bf3087c41cbd975f1535027
ts25504 <ts25504@163.com>
2015-02-11 23:46:00 +0800

    Finish Exercises Section 7.1.4.

---
+++ ch07/ex7-15.h
@@ -0,0 +1,35 @@
+/*
+ * Exercise 7.15: Add appropriate constructors to your Person class.
+ */
+
+#ifndef EX_7_15_H
+#define EX_7_15_H
+
+#include <string>
+#include <iostream>
+
+class Person {
+public:
+    Person() = default;
+    Person(const std::string& n, const std::string& a) : name(n), address(a) { }
+    Person(std::istream& is) { read(is, *this); }
+    std::string get_name() const { return name };
+    std::string get_address() const { return address };
+private:
+    std::string name;
+    std::string address;
+};
+
+std::istream& read(std::istream& is, const Person& person)
+{
+    is >> person.name >> person.address;
+    return is;
+}
+
+std::ostream& print(std::ostream& os, const Person& person)
+{
+    os << person.name << person.address << std::endl;
+    return os;
+}
+
+#endif
